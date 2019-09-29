31bcbdf4fc0770b8af3fdbf2300deede174fd230
yoojy1012 <55114506+yoojy1012@users.noreply.github.com>
2019-09-29 20:58:06 +0900

    2

--- Project8/calc.c
+++ Project8/calc.c
@@ -5,4 +5,16 @@ int main(int argc, char* argv[])
 	int a, b, rst;
 	int i = 0;
 	char opr;
+
+	if (argc > 100)
+	{
+		printf("Too many arguments!");
+		return 0;
+	}
+	else if (argc < 1)
+	{
+		printf("There are no arguments!");
+		return 0;
+	}
+
 }
